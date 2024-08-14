088d4a466f48c9a0a7d03240f3fd8e1cbe543c26
vncc <vncc@gmv.com>
2024-08-14 13:56:53 +0200

    Separate prompt experiments

--- llm_chain/inference.py
+++ llm_chain/inference.py
@@ -5,7 +5,7 @@ from pprint import pprint
 import argparse
 from pathlib import Path
 
-# from experiments.sample1.prompt_template import get_prompt
+from experiments.sample1.prompt_template import get_prompt
 
 
 def read_ontology(file):
@@ -27,32 +27,13 @@ def inference(file):
     
     
 def LLM_consume(db_schema, ontology):
-    prompt = f"""
-        Necesito generar un fichero RML (RDF Mapping Language) que mapee una base de datos relacional a RDF usando una ontología específica.
-
-        ### Ontología:
-        {ontology}
-
-        ### Esquema de la base de datos (en formato CSV):
-        {db_schema}
-
-        ### Requisitos:
-        1. Usa la ontología proporcionada para definir las clases y propiedades en el RML.
-        2. Cada columna del esquema de la base de datos debe mapearse a una propiedad adecuada de la ontología.
-        3. Genera un mapeo para cada tabla en el esquema CSV.
-        4. Asegúrate de definir los TriplesMap, LogicalSource, SubjectMap, PredicateObjectMap, y cualquier otra estructura necesaria de acuerdo con la especificación RML.
-        5. El RML generado debe ser válido y compatible con motores de mapeo RML, como RMLMapper.
-
-        Proporciona el contenido completo del fichero RML en formato texto. No incluyas saltos de linea al comienzo del fichero ni triples comillas
-        """
-    
     headers = {
         'Content-Type': 'application/json',
     }
 
     json_data = {
         'model': 'TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ',
-        'prompt': prompt, #get_prompt(db_schema, ontology),
+        'prompt': get_prompt(db_schema, ontology),
         'max_tokens': 450,
         'temperature': 0,
     }
