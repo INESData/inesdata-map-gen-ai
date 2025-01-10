ad4a220cb58b9b90bbacb22ea53ee995917d8be8
Avleen Kaur <akaur@gmv.com>
2025-01-10 08:54:29 +0000

    Rml metric useage in MLflow

--- gen_ai_mapping/llm_metrics.py
+++ gen_ai_mapping/llm_metrics.py
@@ -17,15 +17,38 @@ from morph_kgc.mapping.mapping_parser import (
     _complete_pom_with_default_graph,
     _complete_termtypes,
     _complete_triples_map_class,
-    _validate_termtypes
+    _validate_termtypes,
 )
 
 from llm_metrics_const import *
 import numpy as np
-from sklearn.metrics import confusion_matrix,classification_report
+from sklearn.metrics import confusion_matrix, classification_report
 from io import StringIO
 
 
+RML_COLS = [
+    rdflib.term.Variable("logical_source_value"),
+    rdflib.term.Variable("reference_formulation"),
+    rdflib.term.Variable("iterator"),
+    rdflib.term.Variable("subject_map_value"),
+    rdflib.term.Variable("predicate_map_type"),
+    rdflib.term.Variable("predicate_map_value"),
+    rdflib.term.Variable("object_map_type"),
+    rdflib.term.Variable("object_map_value"),
+]
+
+RML_COLS_STR = [
+    "logical_source_value",
+    "reference_formulation",
+    "iterator",
+    "subject_map_value",
+    "predicate_map_type",
+    "predicate_map_value",
+    "object_map_type",
+    "object_map_value",
+]
+    
+    
 def get_text_similarity(text1, text2):
     # Descargar recursos necesarios de NLTK
     nltk.download("punkt")
@@ -90,104 +113,57 @@ def convert_to_df(mapping_graph):
     mapping_graph = normalize_mapping_graph(mapping_graph)
     # Parse the mappings using the SPARQL query
     rml_query_results = mapping_graph.query(RML_PARSING_QUERY_MORPH)
-    #print(rml_query_results.bindings)
     # Convert Dict to DataFrame
     rml_df = pd.DataFrame(rml_query_results.bindings)
+    # In csv cases, there is no iterator
+    if rdflib.term.Variable("iterator") not in rml_df.columns:
+        rml_df[rdflib.term.Variable("iterator")] = None
 
     return rml_df
 
 
-def get_rml_metrics(mapping,llm_mapping):
+def get_rml_metrics(mapping, llm_mapping):
     # convert into rdf graph
     mapping_graph = convert_to_rml(mapping)
     # convert into pandas dataframe
     rml = convert_to_df(mapping_graph)
-    # selecting the required columns from rml
-    rml_df = rml[[
-        rdflib.term.Variable('logical_source_value'),
-        rdflib.term.Variable('reference_formulation'), 
-        rdflib.term.Variable('iterator'),  
-        rdflib.term.Variable('subject_map_value'), 
-        rdflib.term.Variable('predicate_map_type'), 
-        rdflib.term.Variable('predicate_map_value'),  
-        rdflib.term.Variable('object_map_type'), 
-        rdflib.term.Variable('object_map_value') 
-        ]].copy()
+    # selecting the required columns from rml    
+    rml_df = rml[RML_COLS].copy()
     # changing the column names to strings
-    rml_df.columns = [
-    'logical_source_value', 
-    'reference_formulation', 
-    'iterator', 
-    'subject_map_value', 
-    'predicate_map_type', 
-    'predicate_map_value', 
-    'object_map_type', 
-    'object_map_value'
-    ]
+    rml_df.columns = RML_COLS_STR
 
-    rows= len(rml_df.index) # number of rows
-    print(rows)
+    rows = len(rml_df.index)  # number of rows
     # dropping last row which is unnecessary
-    rml_df.drop(index=rml_df.index[rows-1],inplace=True)
+    rml_df.drop(index=rml_df.index[rows - 1], inplace=True)
     # convert csv string into dataframe
-    llm_mapping= llm_mapping.strip()
-    llm = pd.read_csv(StringIO(llm_mapping), sep='|')
+    llm_mapping = llm_mapping.strip()
+    if len(llm_mapping) == 0:
+        return 0
+    llm = pd.read_csv(StringIO(llm_mapping), sep="|")
     # selecting the columns from llm df
-    llm_df = llm[[
-        'logical_source_value', 
-        'reference_formulation', 
-        'iterator', 
-        'subject_map_value', 
-        'predicate_map_type', 
-        'predicate_map_value', 
-        'object_map_type', 
-        'object_map_value'
-        ]].copy()
+    llm_df = llm[RML_COLS_STR].copy()
 
     # printing the dataframes
     print("RML_DF:")
-    # print(list(rml.columns.values)) # print all the columns headers
     print(rml_df)
     print("LLM_DF:")
     print(llm_df)
-    #compare two pds
-    
+    # compare two pds
+
     columns = list(rml_df)  # list all the columns
-    f1_list=[]
-    
-    # TODO: comparing the sub-df and llm-df 
+    f1_list = []
+
+    # comparing the sub-df and llm-df
     for i in columns:
-           print(f'COLUMN NAME: {i}')
-           # striping blank spaces if present
-           llm_df[i]=llm_df[i].str.strip()
-           rml_df[i]=rml_df[i].str.strip()
-           # print the Classification report
-           print(classification_report(llm_df[i],rml_df[i]))
-           # compute F1-scores
-           f1=f1_score(llm_df[i],rml_df[i],average='weighted')
-           # add it to the list 
-           f1_list.append(f1)
+        print(f"COLUMN NAME: {i}")
+        # striping blank spaces if present
+        llm_df[i] = llm_df[i].str.strip()
+        rml_df[i] = rml_df[i].str.strip()
+        # print the Classification report
+        print(classification_report(llm_df[i], rml_df[i]))
+        # compute F1-scores
+        f1 = f1_score(llm_df[i], rml_df[i], average="weighted")
+        # add it to the list
+        f1_list.append(f1)
     # return the average f1 score
-    return sum(f1_list)/len(f1_list)
-
-
-# Running the correct experiment
-if __name__ == "__main__":
-    with open(
-        "../data/output/expected/people_expected_RML.rml",
-        # "../data/output/expected/diccionari_casteller_mappings.rml.ttl",
-        # "/home/code/inesdata-map/gen-ai-mapping/data/output/expected/gtfs-csv.rml.ttl",
-        "r",
-    ) as file:
-        rml_string = file.read()
-    
-    llm_string="""
-    logical_source_value|reference_formulation|iterator|subject_map_value|predicate_map_type|predicate_map_value|object_map_type|object_map_value
-    people.csv|http://semweb.mmlab.be/ns/rml#CSV|/rows/row|http://example.org/person/{id}|http://w3id.org/rml/constant|http://example.org/hasName|http://w3id.org/rml/reference|name
-    people.csv|http://semweb.mmlab.be/ns/rml#CSV|/rows/row|http://example.org/person/{id}|http://w3id.org/rml/constant|http://example.org/hasAge|http://w3id.org/rml/reference|age
-    """
-    
-
-    
-    av_f1=get_rml_metrics(rml_string,llm_string)
-    print(f'The average f1 score is: {av_f1}')
+    return sum(f1_list) / len(f1_list)
