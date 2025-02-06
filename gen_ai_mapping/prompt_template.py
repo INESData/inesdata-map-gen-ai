7f37e5a8fbf20192d54423e230a099f454bc6a06
Vanessa Navarro Coronado <vanessa.navarro.coronado@gmv.com>
2025-02-06 09:18:53 +0000

    LLM output adaptation to web

--- gen_ai_mapping/prompt_template.py
+++ gen_ai_mapping/prompt_template.py
@@ -1,27 +1,26 @@
 
 def get_prompt(data_source_schema, ontology):
     prompt = f"""
-        [INST] You are an expert in creating a csv string of Subject, Object and Predicate from Ontologies and Data Sources.
-        The query can consist of Ontologies and Data source schema. You need to analyse them and extract the relevant parts to generate a csv string containing the following column: 
-        ontology|logical_source_value|reference_formulation|iterator|subject_map_value|predicate_map_type|predicate_map_value|object_map_type|object_map_value. 
+        [INST] You are an expert in creating a csv string. The query will consist of Ontologies and Data source schema. 
+        You need to extract the relevant parts to generate a csv string containing the following column: 
+        ontology, logical_source_value, reference_formulation, iterator, subject_map_value, predicate_map_type, predicate_map_value, object_map_type and object_map_value. 
         Identify and map the relevant fields from the ontology and data source schema to the required columns. 
-        For each entry in the ontology and data source schema, extract the values for the mapped fields and if the columns' value is not present then write NONE. 
-        Construct a row for each entry with the extracted values. Combine the rows into a single string, with each value separated by a pipe (|) and each row separated by a newline (\n). 
-        Ensure the first row contains the column headers. Concatenate the header row and the data rows to form the final CSV string. Don't use \n where necessary. 
-
-        Here is an example of the expected output for an ontology and a people data source:
+        For each entry in the ontology and data source schema, extract the values for the mapped fields. 
+        Construct a row with the extracted values and combine the row into a single string, with each value separated by a pipe (|) and each row separated by a newline (\n). 
+        Ensure the first row contains the column headers. Concatenate the header row and the data rows to form the final CSV string.
+ 
+        Here is the format of the expected output for an ontology and a people data source:
         ontology|logical_source_value|reference_formulation|iterator|subject_map_value|predicate_map_type|predicate_map_value|object_map_type|object_map_value
         people.ttl|people.csv|csv|/rows/row|Person|rml:reference|hasName|xsd:string|name
         people.ttl|people.csv|csv|/rows/row|Person|rml:reference|hasAge|xsd:integer|age
-
+ 
         Here is the query from the user. Please analyse and do as described above.
-        Ontologies:
         {ontology}
-
+   
         Data source schema:
         {data_source_schema}
-
-        Just generate the csv string without explanation:
+ 
+        Just generate the csv string.
         [/INST]
     """
     return prompt
